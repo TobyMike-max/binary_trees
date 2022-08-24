fc32516ea59e71914ac762fb3afefa36c0157341
TobyMike-max <osinubi.michael@gmail.com>
2022-08-24 17:11:57 +0100

    [Modified]: Optimized code base

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,20 +15,19 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new, *tmp;
+	binary_tree_t *new;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL || parent == NULL)
+	if (parent == NULL)
 		return (NULL);
-	new->n = value;
-	new->left = NULL;
-	new->right = NULL;
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
 
 	if (parent->left == NULL)
 		parent->left = new;
-	tmp = parent->left;
-	parent->left = new;
-	parent->left->left = tmp;
+	new->left = parent->left;
+	parent->left->parent = new;
 
 	return (new);
 }
